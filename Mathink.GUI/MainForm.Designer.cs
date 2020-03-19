b4b5f3764f787392ecf293e18260a763584fd025
pluveto <i@pluvet.com>
2020-03-20 00:48:21 +0800

    go

---
+++ Mathink.GUI/MainForm.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Mathink.GUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOut = new System.Windows.Forms.PictureBox();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.slErr = new System.Windows.Forms.ToolStripStatusLabel();
+            this.slRet = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.导入ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存图片ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存SVGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.githubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.复制输出路径ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开程序目录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picOut)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picOut
+            // 
+            this.picOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picOut.Location = new System.Drawing.Point(11, 181);
+            this.picOut.Name = "picOut";
+            this.picOut.Padding = new System.Windows.Forms.Padding(8);
+            this.picOut.Size = new System.Drawing.Size(395, 200);
+            this.picOut.TabIndex = 0;
+            this.picOut.TabStop = false;
+            // 
+            // txtIn
+            // 
+            this.txtIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtIn.Location = new System.Drawing.Point(12, 48);
+            this.txtIn.Multiline = true;
+            this.txtIn.Name = "txtIn";
+            this.txtIn.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtIn.Size = new System.Drawing.Size(394, 101);
+            this.txtIn.TabIndex = 1;
+            this.txtIn.Text = "\\vec{x}=\\frac{1}{2}\\vec{a}t^2";
+            this.txtIn.TextChanged += new System.EventHandler(this.txtIn_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "预览";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "公式编辑";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.slErr,
+            this.slRet});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 368);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(423, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // slErr
+            // 
+            this.slErr.ForeColor = System.Drawing.Color.Red;
+            this.slErr.Name = "slErr";
+            this.slErr.Size = new System.Drawing.Size(0, 17);
+            // 
+            // slRet
+            // 
+            this.slRet.Name = "slRet";
+            this.slRet.Size = new System.Drawing.Size(0, 17);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.编辑ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(423, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.导入ToolStripMenuItem,
+            this.保存ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            this.文件ToolStripMenuItem.Click += new System.EventHandler(this.文件ToolStripMenuItem_Click);
+            // 
+            // 导入ToolStripMenuItem
+            // 
+            this.导入ToolStripMenuItem.Name = "导入ToolStripMenuItem";
+            this.导入ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.导入ToolStripMenuItem.Text = "打开";
+            this.导入ToolStripMenuItem.Click += new System.EventHandler(this.导入ToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.保存图片ToolStripMenuItem,
+            this.保存SVGToolStripMenuItem,
+            this.保存ToolStripMenuItem1});
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            // 
+            // 保存图片ToolStripMenuItem
+            // 
+            this.保存图片ToolStripMenuItem.Name = "保存图片ToolStripMenuItem";
+            this.保存图片ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存图片ToolStripMenuItem.Text = "保存 PNG 图片";
+            this.保存图片ToolStripMenuItem.Click += new System.EventHandler(this.保存图片ToolStripMenuItem_Click);
+            // 
+            // 保存SVGToolStripMenuItem
+            // 
+            this.保存SVGToolStripMenuItem.Name = "保存SVGToolStripMenuItem";
+            this.保存SVGToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.保存SVGToolStripMenuItem.Text = "保存 SVG 图片";
+            this.保存SVGToolStripMenuItem.Click += new System.EventHandler(this.保存SVGToolStripMenuItem_Click);
+            // 
+            // 保存ToolStripMenuItem1
+            // 
+            this.保存ToolStripMenuItem1.Name = "保存ToolStripMenuItem1";
+            this.保存ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.保存ToolStripMenuItem1.Text = "保存公式文本";
+            this.保存ToolStripMenuItem1.Click += new System.EventHandler(this.保存ToolStripMenuItem1_Click);
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.githubToolStripMenuItem,
+            this.复制输出路径ToolStripMenuItem,
+            this.打开程序目录ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // githubToolStripMenuItem
+            // 
+            this.githubToolStripMenuItem.Name = "githubToolStripMenuItem";
+            this.githubToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.githubToolStripMenuItem.Text = "Github 首页";
+            // 
+            // 复制输出路径ToolStripMenuItem
+            // 
+            this.复制输出路径ToolStripMenuItem.Name = "复制输出路径ToolStripMenuItem";
+            this.复制输出路径ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.复制输出路径ToolStripMenuItem.Text = "复制输出路径";
+            this.复制输出路径ToolStripMenuItem.Click += new System.EventHandler(this.复制输出路径ToolStripMenuItem_Click);
+            // 
+            // 打开程序目录ToolStripMenuItem
+            // 
+            this.打开程序目录ToolStripMenuItem.Name = "打开程序目录ToolStripMenuItem";
+            this.打开程序目录ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.打开程序目录ToolStripMenuItem.Text = "打开程序目录";
+            this.打开程序目录ToolStripMenuItem.Click += new System.EventHandler(this.打开程序目录ToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(423, 390);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.picOut);
+            this.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Mathink 编辑器";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picOut)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOut;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel slErr;
+        private System.Windows.Forms.ToolStripStatusLabel slRet;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 导入ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存图片ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存SVGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem githubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 复制输出路径ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开程序目录ToolStripMenuItem;
+    }
+}
+
---
+++ Mathink.GUI/MainForm.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WpfMath;
+using WpfMath.Converters;
+
+namespace Mathink.GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public string OutputPath { get; private set; } = "d.out.txt";
+        private void txtIn_TextChanged(object sender, EventArgs e)
+        {
+            slRet.Text = string.Empty;
+            var latex = txtIn.Text.Trim();
+            if (latex == string.Empty) return;
+            string d;
+            try
+            {
+                (d, imgOut) = renderLatex(latex);
+            }
+            catch (Exception ex)
+            {
+                slErr.Text = "错误: " + ex.Message;
+                return;
+            }
+            File.WriteAllText(OutputPath, d);
+            using (var ms = new MemoryStream(imgOut))
+            {
+                this.picOut.Image = Image.FromStream(ms);
+            }
+            slErr.Text = string.Empty;
+            slRet.Text = "渲染成功";
+
+        }
+        byte[] imgOut;
+
+
+
+        private static (string, byte[]) renderLatex(string latex, bool onlyReturnDpath = true)
+        {
+
+            var parser = new TexFormulaParser();
+            var formula = parser.Parse(latex);
+            var renderer = formula.GetRenderer(TexStyle.Display, 20, "Arial");
+            var pngBytes = formula.RenderToPng(20.0, 0.0, 0.0, "Arial");
+            var geometry = renderer.RenderToGeometry(0, 0);
+            var converter = new SVGConverter();
+            var svgPathText = converter.ConvertGeometry(geometry);
+            if (!onlyReturnDpath) return (AddSVGHeader(svgPathText), pngBytes);
+            var tmp1 = svgPathText.Split(new string[] { "d=\"" }, StringSplitOptions.None)[1];
+            var tmp2 = tmp1.Split(new string[] { "\"" }, StringSplitOptions.None)[0];
+            return (tmp2, pngBytes);
+        }
+        private static string AddSVGHeader(string svgText)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
+                .AppendLine("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" >")
+                .AppendLine(svgText)
+                .AppendLine("</svg>");
+
+            return builder.ToString();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            txtIn_TextChanged(null, null);
+        }
+
+        private void 文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 导入ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "打开文件";
+            dialog.FileName = "";
+            dialog.Filter = "平文本(.txt)|.txt|Markdown(.md)|.md|LaTex(.tex)|.tex|所有(*.*)|*.*";
+
+            if (dialog.ShowDialog() == DialogResult.Cancel) return;
+            var fileName = dialog.FileName;
+            if (!File.Exists(fileName))
+            {
+                MessageBox.Show("文件不存在!", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            txtIn.Text = File.ReadAllText(fileName);
+
+        }
+
+        private void 保存图片ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "";
+            dialog.Filter = "PNG 格式|.png";
+            dialog.Title = "保存";
+            if (dialog.ShowDialog() == DialogResult.Cancel) return;
+
+            var fileName = dialog.FileName;
+            if (!fileName.EndsWith(".png")) fileName += ".png";
+            System.IO.File.WriteAllBytes(fileName, imgOut);
+            var ret = MessageBox.Show("保存成功, 打开所在目录吗?", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (ret == DialogResult.Yes)
+            {
+                string argument = "/select, \"" + fileName + "\"";
+                System.Diagnostics.Process.Start("explorer.exe", argument);
+            }
+        }
+
+        private void 保存SVGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string svg;
+            var latex = txtIn.Text.Trim();
+            (svg, _) = renderLatex(latex, false);
+
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "";
+            dialog.Filter = "SVG 格式(.svg)|.svg";
+            dialog.Title = "保存";
+            if (dialog.ShowDialog() == DialogResult.Cancel) return;
+
+            var fileName = dialog.FileName;
+            if (!fileName.EndsWith(".svg")) fileName += ".svg";
+            System.IO.File.WriteAllText(fileName, svg);
+            var ret = MessageBox.Show("保存成功, 打开所在目录吗?", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (ret == DialogResult.Yes)
+            {
+                string argument = "/select, \"" + fileName + "\"";
+                System.Diagnostics.Process.Start("explorer.exe", argument);
+            }
+
+        }
+
+        private void 保存ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            var latex = txtIn.Text.Trim();
+
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "";
+            dialog.Filter = "平文本(.txt)|.txt";
+            dialog.Title = "保存";
+            if (dialog.ShowDialog() == DialogResult.Cancel) return;
+
+            var fileName = dialog.FileName;
+            if (!fileName.EndsWith(".txt")) fileName += ".txt";
+            System.IO.File.WriteAllText(fileName, latex);
+            var ret = MessageBox.Show("保存成功, 打开所在目录吗?", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (ret == DialogResult.Yes)
+            {
+                string argument = "/select, \"" + fileName + "\"";
+                System.Diagnostics.Process.Start("explorer.exe", argument);
+            }
+        }
+
+        private void 复制输出路径ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(Application.StartupPath + Path.DirectorySeparatorChar + OutputPath);
+            slRet.Text = "复制成功!";
+        }
+
+        private void 打开程序目录ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", Application.StartupPath);
+        }
+    }
+}
