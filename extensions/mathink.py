f1cdbc5f89d37d2d19fcc4b2f3a2f7d8b9a93908
pluveto <50045289+pluveto@users.noreply.github.com>
2020-09-09 22:40:49 +0800

    Adapt for inkscape 1.0

--- extensions/mathink.py
+++ extensions/mathink.py
@@ -5,6 +5,7 @@ import os
 import inkex
 import re
 import copy
+from lxml import etree
 # from inkex.elements import load_svg, Group
 
 svgstr = ""
@@ -15,21 +16,22 @@ SVG_NS = u"http://www.w3.org/2000/svg"
 def process_exists(process_name):
     call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
     # use buildin check_output right away
-    output = subprocess.check_output(call)
+    output = subprocess.check_output(call, shell=True)
+    # inkex.debug(output.decode("ansi"))
     # check in last line for process name
-    last_line = output.strip().split('\r\n')[-1]
+    last_line = output.decode("ansi").strip().split('\r\n')[-1]
     # because Fail message could be translated
     return last_line.lower().startswith(process_name.lower())
 
 class Mathink(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
-        self.OptionParser.add_option("--out",
-                        action="store", type="string", 
+        self.arg_parser.add_argument("--out",
+                        action="store", type=str, 
                         dest="out", 
                         help="out svg file path")
-        self.OptionParser.add_option(
-            "-s", "--scale", action="store", type="float",
+        self.arg_parser.add_argument(
+            "-s", "--scale", action="store", type=float,
             dest="scale",
             default=1.0)
 
@@ -41,7 +43,7 @@ class Mathink(inkex.Effect):
     def effect(self):
         if not os.path.isfile(self.options.out):
             sys.exit()
-        newnode = inkex.etree.parse(self.options.out).getroot()
+        newnode = etree.parse(self.options.out).getroot()
         scale = self.options.scale
         
         # g = Group.create(self.options.parent, True)
@@ -50,13 +52,13 @@ class Mathink(inkex.Effect):
         # self.current_layer.append(g)
         # extDocument = load_svg(svgstr)
         svg = self.document.getroot()
-        g = inkex.etree.SubElement(svg, 'g')        
+        g = etree.SubElement(svg, 'g')        
         g.append(newnode)
         if scale is not None:
             g.attrib['transform'] = 'matrix(%f,0,0,%f,%f,%f)' % (
                 self.options.scale, self.options.scale,
-                self.view_center[0],
-                self.view_center[1])
+                self.svg.namedview.center[0],
+                self.svg.namedview.center[1])
             #inkex.debug(g.attrib['transform'])
         # self.current_layer.append(g)
         
@@ -74,7 +76,7 @@ if __name__ == '__main__':
     argv.insert(0, output_file)
     
     if not process_exists("Mathink.GUI.exe"):
-        subprocess.Popen("explorer.exe " + os.path.abspath(os.path.dirname(output_file) + "/Mathink.GUI.exe"))
+        os.startfile(os.path.abspath(os.path.dirname(output_file) + "/Mathink.GUI.exe"))
         sys.exit()
     if not os.path.isfile(output_file):
         inkex.debug("file not found: " + output_file)
@@ -83,4 +85,4 @@ if __name__ == '__main__':
     #     svgstr = file.read()
 
     e = Mathink()
-    e.affect()
+    e.run()
